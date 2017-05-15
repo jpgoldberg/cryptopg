cc62f562a7535a772fb964b330f6b9d37ef4f34c
Jeffrey Goldberg <jeff@agilebits.com>
2017-05-15 00:53:59 -0500

    comments, formating, tidying.

--- dprng/dprng.go
+++ dprng/dprng.go
@@ -1,4 +1,6 @@
-// Package dprng provides a deterministic (seedable) pseudo-random number generator
+// Package dprng provides a deterministic (seedable) pseudo-random number generato.
+// I do not know why I picked OFB mode for the steam cipher.
+// CTR would probably have been faster.
 package dprng
 
 import (
@@ -8,13 +10,14 @@ import (
 )
 
 // DPRNG is our deterministic Pseudo-Random Byte Generator
+// Public interface is through functions only
 type DPRNG struct {
 	block     cipher.Block
 	blockSize int
 	stream    cipher.Stream
 }
 
-// NewDPRNG sets up a new DPRNG with seed
+// NewDPRNG sets up a new DPRNG with seed, which must be a valid AES key size
 // This uses AES in OFB mode to create a stream that just encrypts zeros
 func NewDPRNG(seed []byte) (d *DPRNG, err error) {
 	d = new(DPRNG)
@@ -22,26 +25,26 @@ func NewDPRNG(seed []byte) (d *DPRNG, err error) {
 	if !(len(seed) == 16 || len(seed) == 24 || len(seed) == 32) {
 		return nil, fmt.Errorf("bad seed length: %d", len(seed))
 	}
+
 	d.block, err = aes.NewCipher(seed)
 	if err != nil {
 		return nil, err
 	}
-
 	d.blockSize = d.block.BlockSize()
+
 	// use 0 as iv and plaintext(security is in the seed)
 	iv := make([]byte, d.blockSize)
-
 	d.stream = cipher.NewOFB(d.block, iv[:])
 
 	return d, nil
 
 }
 
-// Read from the RNG.
+// Read from the RNG into the result. Returns the size in bytes of the result
 func (d DPRNG) Read(result []byte) (n int, err error) {
 
-	length := len(result)
-	src := make([]byte, length)
+	n = len(result)
+	src := make([]byte, n)
 	d.stream.XORKeyStream(result, src)
-	return length, nil
+	return n, nil
 }
